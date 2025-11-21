9d3801e0465d7f85a0f49e0ebbeed8885e6baa04
Alexander Wagner <alexander.wagner@ulal.de>
2025-11-21 22:29:58 +0100

    Init

---
+++ main.py
@@ -0,0 +1,67 @@
+import argparse
+import asyncio
+import dateutil
+from google.cloud import storage
+import json
+import os
+import sys
+from telegram import constants, Bot
+import yaml
+
+
+def _fetch_osv_updated_list(config_osv: dict) -> list:	
+	client = storage.Client.create_anonymous_client()
+	
+	bucket = client.bucket(config_osv["bucket"])
+	print("bucket.exists", bucket.exists())
+	if bucket.exists() is False:
+		exit(-1)
+	
+	blob = bucket.get_blob(config_osv["blob"])
+	print("blob.exists", blob.exists())
+	if blob.exists() is False:
+		exit(-1)
+	
+	text = blob.download_as_text()
+	lines = text.split("\n")
+	return lines
+
+def _fetch_mal_ids(config: dict, osv_updated_list: list) -> list:
+	cves = []
+	for line in osv_updated_list:
+		if "MAL-" not in line:
+			continue
+	
+		timestamp, mal_package_cve = line.split(",")
+		ecosystem, cve = mal_package_cve.split("/")
+
+		timestamp = dateutil.parser.parse(timestamp)
+		if config["last_timestamp"] > timestamp:
+			break
+	
+		cves.append(cve)
+	config["last_timestamp"] = dateutil.parser.parse(osv_updated_list[0].split(",")[0])
+	return cves
+
+def main() -> int:
+	parser = argparse.ArgumentParser(prog="Malicous package monitor")
+	parser.add_argument('config', help='Config file in yaml format')
+	args = parser.parse_args()
+
+	with open(args.config, "r") as file:
+		config = yaml.safe_load(file)
+
+	osv_updated_list = _fetch_osv_updated_list(config["osv"])
+	cves = _fetch_mal_ids(config, osv_updated_list)
+
+	msg = f"New malicous packages:\n\n```json\n{json.dumps(cves, indent=4)}\n```"
+	bot = Bot(os.environ["BOT_TOKEN"])
+	asyncio.run(bot.send_message(config["telegram"]["channel_id"], msg, parse_mode=constants.ParseMode.MARKDOWN_V2))
+
+	with open(args.config, 'w') as outfile:
+		yaml.dump(config, outfile)
+
+	return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
