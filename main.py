adb482dc4a391d59a917551c28805b1045028e68
Alexander Wagner <alexander.wagner@ulal.de>
2025-11-22 12:39:17 +0100

    main: Do not send empty msg

--- main.py
+++ main.py
@@ -52,6 +52,10 @@ def main() -> int:
 	osv_updated_list = _fetch_osv_updated_list(config["osv"])
 	cves = _fetch_mal_ids(config, osv_updated_list)
 
+	print("len(cves)", len(cves))
+	if cves == []:
+		return 0
+
 	msg = f"New malicious packages:\n\n```json\n{json.dumps(cves, indent=4)}\n```"
 	bot = Bot(os.environ["BOT_TOKEN"])
 	asyncio.run(bot.send_message(config["telegram"]["channel_id"], msg, parse_mode=constants.ParseMode.MARKDOWN_V2))
